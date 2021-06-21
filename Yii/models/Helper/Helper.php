a7447ff9b019611de0c7c9544de67a8864bae39a
illya tishkevich <illya.tishkevich@g.magenmagic.com>
2021-06-21 13:10:18 +0200

    grid fix

--- Yii/models/Helper/Helper.php
+++ Yii/models/Helper/Helper.php
@@ -12,10 +12,21 @@ class Helper
 
         try {
             $messageArray = json_decode($message);
-            $result = isset($messageArray->message)
-                ? $messageArray->message
-                : \yii\helpers\StringHelper::truncate($message, 64);
-
+            if(isset($messageArray->message)) {
+                if(is_array($messageArray->message)) {
+                    $result = \yii\helpers\StringHelper::truncate(
+                        implode($messageArray->message, PHP_EOL),
+                        64
+                    );
+                } else {
+                    $result = $result = \yii\helpers\StringHelper::truncate(
+                        $messageArray->message,
+                        64
+                    );
+                }
+            } else {
+                $result = \yii\helpers\StringHelper::truncate($message, 64);
+            }
         } catch (\Exception $e) {
             $result = \yii\helpers\StringHelper::truncate($message, 64);
         }
