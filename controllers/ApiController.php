2cc1a7841bd8b48e0d0b020e874288df62e9172b
illya tishkevich <illya.tishkevich@g.magenmagic.com>
2023-01-18 01:10:40 +0300

    fix cors

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -55,6 +55,29 @@ class ApiController extends ActiveController
         throw new NotFoundHttpException('The requested page does not exist.');
     }
 
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+
+        $auth = $behaviors['authenticator'];
+        unset($behaviors['authenticator']);
+
+        $behaviors['corsFilter'] = [
+            'class' => \yii\filters\Cors::className(),
+            'cors' => [
+                'Access-Control-Allow-Origin' => ['*'],
+                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+                'Access-Control-Request-Headers' => ['*'],
+                'Access-Control-Allow-Credentials' => true,
+                'Access-Control-Max-Age' => 86400,
+                'Access-Control-Expose-Headers' => [],
+            ]
+        ];
+        $behaviors['authenticator'] = $auth;
+
+        return $behaviors;
+    }
+
     public function actionLog()
     {
         $request                    = Yii::$app->request;
